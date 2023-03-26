a10f882090989d7a8f2badff4d843cacd31fc8f9
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-03-26 12:48:08 -0700

    delete picrew stuff since that plan won't work

--- Program.cs
+++ Program.cs
@@ -1,64 +1,21 @@
 ﻿using System.Net.Http;
 using System.Text.Json;
-const string picrewUrl = "https://picrew.me";
+const string basePath = @"C:\Users\dninemfive\Documents\workspaces\misc\grp";
 HttpClient client = new();
-void Print(JsonProperty prop, int indent = 0)
+async Task Download(string url, string targetPath)
 {
-    string indents = "";
-    for (int i = 0; i < indent; i++) indents += "  ";
-    if (prop.Name == "url") Console.WriteLine($"{indents}https://picrew.me{prop.Value}");
-    else
-    {
-        Console.WriteLine($"{indents}{prop.Name}");
-        foreach(JsonProperty child in prop.Value.EnumerateObject())
-        {
-            Print(child, indent + 1);
-        }
-    }
-}
-async Task Acquire(JsonProperty prop, string? previousPath = null, int indent = 0)
-{
-    previousPath ??= "C:/Users/dninemfive/Documents/workspaces/misc/grp/files/";
-    string indents = "";
-    for (int i = 0; i < indent; i++) indents += "  ";
-    if(prop.Name == "url")
-    {
-        string fileName = Path.GetFileName(prop.Value.ToString());
-        string fullPath = Path.Join(previousPath, fileName);
-        if(File.Exists(fullPath))
-        {
-            Console.WriteLine($"{indents}{fileName} already exists.");
-            return;
-        }
-        await Download(prop.Value.ToString(), fullPath, indents);
-    } 
-    else
-    {
-        Console.WriteLine($"{indents}{prop.Name}");
-        string fullPath = Path.Join(previousPath, prop.Name);
-        Directory.CreateDirectory(fullPath);
-        foreach (JsonProperty child in prop.Value.EnumerateObject()) await Acquire(child, fullPath, indent + 1);
-    }    
-}
-async Task Download(string urlPath, string targetPath, string indents = "")
-{
-    string url = $"{picrewUrl}{urlPath}";
-    Console.WriteLine($"{indents}{Path.GetFileName(targetPath)} does not exist. Downloading from {url}...");
+    Console.WriteLine($"Downloading {url} to {targetPath}...");
     using HttpResponseMessage response = await client.GetAsync(url);
     if(response.IsSuccessStatusCode)
     {
         using Stream stream = await response.Content.ReadAsStreamAsync();
         using FileStream fs = new(targetPath, FileMode.Create);
         await stream.CopyToAsync(fs);
-        Console.WriteLine($"{indents}  Successfully saved to {targetPath}");
+        Console.WriteLine($"\t✔️\t{response.StatusCode} {response.ReasonPhrase}");
     }
     else
     {
-        Console.WriteLine($"{indents}  Download failed: {response.StatusCode} {response.ReasonPhrase}");
+        Console.WriteLine($"\t❌\t{response.StatusCode} {response.ReasonPhrase}");
     }
 }
-JsonDocument doc = JsonDocument.Parse(File.OpenRead("C:/Users/dninemfive/Documents/picrew files.json"));
-foreach (JsonProperty prop in doc.RootElement.EnumerateObject())
-{
-    await Acquire(prop);
-}
+await Download("https://cdn.discordapp.com/attachments/771462457436667935/1089635920661196860/Untitled.jpg", @$"{basePath}\test.png");
