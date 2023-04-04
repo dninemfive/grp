65e55f0c59680bb120b745cb0d1ff6d3ca5e951d
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-04-04 12:45:19 -0700

    json working

--- Variables/Config.cs
+++ Variables/Config.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 #pragma warning disable CS8618
@@ -12,21 +13,50 @@ namespace grp
         public static Config Current;
         public class ConfigPaths
         {
+            [JsonInclude]
             public string BaseFolder;
+            [JsonInclude]
             public string ImageFolder = "/images/";
+            [JsonInclude]
             public string DebugFolder = "/debug/";
+            [JsonInclude]
             public string TsvFile = "data.tsv";
+            [JsonInclude]
             public string WatermarkToSubtract = "watermark_subtract.png";
+            [JsonInclude]
             public string WatermarkToAdd = "watermark_add.png";
+            [JsonInclude]
             public string? GoogleAuthKey = null;
+            public ConfigPaths() { }
+            /*
+            [JsonConstructor]
+            public ConfigPaths(string baseFolder, string imageFolder, string debugFolder, 
+                string tsvFile, string watermarkToSubtract, string watermarkToAdd, string? googleAuthKey)
+            {
+                BaseFolder = baseFolder;
+                ImageFolder = imageFolder;
+                DebugFolder = debugFolder;
+                TsvFile = tsvFile;
+                WatermarkToSubtract = watermarkToSubtract;
+                WatermarkToAdd = watermarkToAdd;
+                GoogleAuthKey = googleAuthKey;
+            }*/
         }
+        [JsonInclude]
         public ConfigPaths Paths;
+        [JsonInclude]
         public bool Debug = false;
+        [JsonInclude]
         public bool SavePerUserImages = false;
+        [JsonInclude]
         public bool CopyDescToClipboard = true;
+        [JsonInclude]
         public bool SaveDescToFile = false;
-        public string? GoogleAuthEmail = null;        
+        [JsonInclude]
+        public string? GoogleAuthEmail = null;
+        [JsonInclude]
         public string? GoogleFileId = null;
+        [JsonInclude]
         public int MaxUsersPerRow = 12;
     }
 }
